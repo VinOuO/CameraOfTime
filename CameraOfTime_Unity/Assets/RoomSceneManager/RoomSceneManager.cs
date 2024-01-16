87a9762236f5a073cdfb37dec690ddcda5bd174e
root <root@eae-z490-124.ad.utah.edu>
2024-01-16 12:49:02 -0700

    Add RoomSceneManager

---
+++ CameraOfTime_Unity/Assets/RoomSceneManager/RoomSceneManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomSceneManager : MonoBehaviour
+{
+    public RoomScene CurrentRoomScene = RoomScene.PersentRoom;
+    void Start()
+    {
+        InitRoomScene();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SwitchRoomScene(SwitchMode.Pervious);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SwitchRoomScene(SwitchMode.Next);
+        }
+    }
+
+    public void InitRoomScene()
+    {
+        CurrentRoomScene++;
+        CurrentRoomScene = (RoomScene)((int)CurrentRoomScene % (int)RoomScene.Max);
+        Debug.Log(CurrentRoomScene);
+        SceneManager.LoadScene(CurrentRoomScene.ToString(), LoadSceneMode.Additive);
+    }
+
+    public void SwitchRoomScene(SwitchMode mode)
+    {
+        if (CurrentRoomScene != RoomScene.None)
+        {
+            SceneManager.UnloadSceneAsync(CurrentRoomScene.ToString(), UnloadSceneOptions.None);
+        }
+        CurrentRoomScene += (mode == SwitchMode.Pervious) ? -1 : 1;
+        CurrentRoomScene = (RoomScene)(((int)CurrentRoomScene + (int)RoomScene.Max) % (int)RoomScene.Max);
+        Debug.Log(CurrentRoomScene);
+        SceneManager.LoadScene(CurrentRoomScene.ToString(), LoadSceneMode.Additive);
+    }
+
+    public enum SwitchMode
+    {
+        Pervious = 1,
+        Next = 2,
+    }
+
+    public enum RoomScene
+    {
+        None = -1,
+        PastRoom = 0,
+        PersentRoom = 1,
+        FutureRoom = 2,
+        Max = 3,
+    }
+}
